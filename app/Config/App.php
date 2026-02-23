0fe8a2e51a3e09682c0d140eed78c48b574b0275
selvihdy31 <selvihdy31@gmail.com>
2026-02-23 23:09:53 +0700

    update

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://absensidinsos.mnz.dom.my.id/';
+    public string $baseURL = 'https://welcome-record-wov.mnz.dom.my.id/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
